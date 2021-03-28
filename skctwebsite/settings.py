9b28f4f08b38e54f230a051d49bf84496e86de85
nithish <55284784+NithishC@users.noreply.github.com>
2021-03-28 11:32:30 +0530

    Update settings.py

--- skctwebsite/settings.py
+++ skctwebsite/settings.py
@@ -59,9 +59,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'skctwebsite.urls'
+
 
 TEMPLATES = [
     {
@@ -144,6 +146,8 @@ SOCIALACCOUNT_PROVIDERS = { 'google':
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 AUTHENTICATION_BACKENDS = (
